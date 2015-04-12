57b0290ffac03291f44346ce5d87496669592487
bound1ess <bound1ess.dev@gmail.com>
2015-04-12 18:48:47 +0600

    Test TaskManager's constructor method.

--- spec/todo/task_manager_spec.rb
+++ spec/todo/task_manager_spec.rb
@@ -7,6 +7,16 @@ RSpec.describe(Todo::TaskManager) do
         @sut = Todo::TaskManager.new(@path)
     end
 
+    it('raises an error if an invalid argument was supplied to the constructor') do
+        expect {
+            Todo::TaskManager.new('foo')
+        }.not_to(raise_error)
+
+        expect {
+            Todo::TaskManager.new(123)
+        }.to raise_error(ArgumentError, 'Expected a string, got fixnum.')
+    end
+
     after(:each) do
         File.delete(@path) if File.exists?(@path)
     end
