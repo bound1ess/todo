27279b0c5e2bcd6424e32bd40aa919a1a38822ab
bound1ess <bound1ess.dev@gmail.com>
2015-04-12 17:27:53 +0600

    Cover all untested methods in Queue.

--- spec/todo/containers/queue_spec.rb
+++ spec/todo/containers/queue_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe(Todo::Containers::Queue) do
     before(:each) do
         @sut = Todo::Containers::Queue.new(2)
     end
+
+    it('pops a value from the queue') do
+        expect {
+            @sut.pop!
+        }.to raise_error(RuntimeError, 'The queue is empty.')
+
+        @sut.push! 1
+        @sut.push! 2
+
+        expect(@sut.pop!).to eq(1)
+        expect(@sut.pop!).to eq(2)
+    end
+
+    it('returns the top element of the queue') do
+        expect(@sut.peek).to eq(nil)
+
+        @sut.push! 1
+        @sut.push! 2
+
+        expect(@sut.peek).to eq(1)
+    end
 end
